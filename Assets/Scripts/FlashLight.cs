63bf7fccbaf993108e169bb5ee74a82eb795f044
chris <540218@escg.ac.uk>
2025-05-01 10:27:53 +0100

    asd

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -1,36 +1,99 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlashLight : MonoBehaviour
 {
-    [SerializeField] GameObject FlashlightLight;
-    private bool FlashlightActive = false;
+    [Header("Flashlight Settings")]
+    [SerializeField] GameObject flashlightLight;
+    [SerializeField] float batteryDrainRate = 1f; // seconds per unit of battery drain
+    [SerializeField] float maxBatteryHealth = 100f;
 
+    [Header("Battery Pickup Settings")]
+    [SerializeField] float batteryRefillAmount = 25f;
+    [SerializeField] float interactRange = 2f;
+    [SerializeField] LayerMask batteryLayer;
 
+    [Header("UI")]
+    [SerializeField] Slider batteryHealthSlider;
 
+    private float currentBatteryHealth;
+    private bool flashlightActive = false;
+    private float drainTimer;
 
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        FlashlightLight.gameObject.SetActive(false);
+        flashlightLight.gameObject.SetActive(false);
+        currentBatteryHealth = maxBatteryHealth;
+
+        if (batteryHealthSlider != null)
+        {
+            batteryHealthSlider.maxValue = maxBatteryHealth;
+            batteryHealthSlider.value = currentBatteryHealth;
+        }
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        HandleFlashlightToggle();
+        HandleBatteryDrain();
+        HandleBatteryPickup();
+    }
+
+    void HandleFlashlightToggle()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (FlashlightActive == false)
+            flashlightActive = !flashlightActive;
+            flashlightLight.SetActive(flashlightActive);
+        }
+    }
+
+    void HandleBatteryDrain()
+    {
+        if (flashlightActive && currentBatteryHealth > 0f)
+        {
+            drainTimer += Time.deltaTime;
+            if (drainTimer >= batteryDrainRate)
             {
-                FlashlightLight.gameObject.SetActive(true);
-                FlashlightActive = true;
-            }
-            else
-            {
-                FlashlightLight.gameObject.SetActive(false);
-                FlashlightActive = false;
+                currentBatteryHealth = Mathf.Max(0f, currentBatteryHealth - 1f);
+                drainTimer = 0f;
+
+                if (batteryHealthSlider != null)
+                    batteryHealthSlider.value = currentBatteryHealth;
+
+                if (currentBatteryHealth <= 0f)
+                {
+                    flashlightLight.SetActive(false);
+                    flashlightActive = false;
+                }
             }
         }
+    }
+
+    void HandleBatteryPickup()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Collider[] hits = Physics.OverlapSphere(transform.position, interactRange, batteryLayer);
+            foreach (var hit in hits)
+            {
+                if (hit.CompareTag("Battery"))
+                {
+                    currentBatteryHealth = Mathf.Min(maxBatteryHealth, currentBatteryHealth + batteryRefillAmount);
+
+                    if (batteryHealthSlider != null)
+                        batteryHealthSlider.value = currentBatteryHealth;
+
+                    Destroy(hit.gameObject);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, interactRange);
     }
 }
